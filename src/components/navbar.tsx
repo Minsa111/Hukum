4d5025f9d17ef0c1800981ef6fb5134d429f443e
Minsa111 <dragonriderrgt@gmail.com>
2025-09-16 22:30:16 +0700

    style: fix navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
 import { NavbarMenu } from "./navbar-menu";
-
+import { CircleUser } from "lucide-react";
 export function Navbar() {
+    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
     return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="/images/logo.png" className="mx-auto w-16 h-auto" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-primary px-2">DIDKDASMEN PDM BATU</span>
             </a>
             <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button" className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
